df6be29db63a19f564c64beeb896f8a688ae9b94
nashokota <preom.cc.bd@gmail.com>
2025-09-21 16:18:15 +0600

    All tasks are done

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        IEBook ebook = new ProxyEBook("DesignPatterns");
 
+        System.out.println("Student 1 accessing eBook...");
+        ebook.display();
+
+        System.out.println("Student 2 accessing eBook...");
+        ebook.display();
+
+    }
+}
