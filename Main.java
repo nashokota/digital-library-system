ec0106b8e9e74344fe3250c165ee25db42641627
nashokota <preom.cc.bd@gmail.com>
2025-09-21 16:16:26 +0600

    Implementing the task in feature branch

--- Main.java
+++ Main.java
@@ -1,14 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
-        IEBook ebook = new ProxyEBook("DesignPatterns.pdf");
 
-        System.out.println("Student 1 accessing eBook...");
-        ebook.display(); // Should trigger loading
-
-        System.out.println("Student 2 accessing eBook...");
-        ebook.display(); // Should NOT trigger loading again
-
-        System.out.println("Student 3 accessing eBook...");
-        ebook.display(); // Should NOT trigger loading again
-    }
-}
